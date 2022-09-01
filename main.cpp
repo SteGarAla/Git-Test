1ee91fcaf1358105e3f3ad524dba166191367d50
stegarala <stegarc@ut.utm.edu>
2022-08-31 22:32:17 -0500

    Fourth Commit: Modified main.cpp

--- main.cpp
+++ main.cpp
@@ -3,9 +3,30 @@
 
 using namespace std;
 
+class sol{
+public:
+  static int amountBetween(int num){
+    int sum = 0;
+    if(num <= 0){
+      return sum;
+    }
+    else{
+      for(int i = 0; i < num; i++){
+	sum += i + 1;
+      }
+    }
+    return sum; 
+  }
+};
+
 int main() {
 
-  cout << "Hello world!" << endl;
+  int userNum;
+  cout << "user, give me number plz" << endl;
+  cin >> userNum;
+
+  cout <<"The sum of 1 through " << userNum << " is " << sol::amountBetween(userNum) << endl;
+  
   
   return 0;
 }
